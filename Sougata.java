290497a8a113829c92e4a0fc3e34189a6ce6993e
dev-debasis <debasiskhamari7@gmail.com>
2023-06-24 12:24:59 +0530

    Second Commit

---
+++ Sougata.java
@@ -0,0 +1,81 @@
+    public class Sougata {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 && i <= (n - 1) / 2 || i == (n - 1) / 2 || j == n - 1 && i >= (n - 1) / 2 
+                        || i == n - 1) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("  ");
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) { 
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("  ");
+
+            for (int j = 0; j < n; j++) {
+                if (j == 0 && i < n - 1 || j == n - 1 && i < n - 1 || i == n - 1 && j > 0 && j < n - 1) { 
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("  ");
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j > 0 && j < n - 1 || j == 0 && i > 0 && i < n - 1 || i == n - 1 && j > 0 && j <= n - 1
+                        || j == n - 1 && i > (n - 1) / 2) { 
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("  ");
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j > 0 && j < n - 1 || j == 0 && i > 0 || i == (n - 1) / 2 || j == n - 1 && i > 0) { 
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("  ");
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == (n - 1) / 2) { 
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("  ");
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j > 0 && j < n - 1 || j == 0 && i > 0 || i == (n - 1) / 2 || j == n - 1 && i > 0) { 
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
